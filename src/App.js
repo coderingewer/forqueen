f6a76dca1521a6ca7b8c1c63df753d52af227e21
yucoobycode <yatli5645@gmail.com>
2023-04-10 00:47:00 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import video from "./video.webm";
+
+function App() {
+  return (
+    <div style={{backgroundColor:"white", height:"100vh", width:"100%"}}>
+    <div className="happy">SEN BENİM DÜNYAMDAKİ TEK KRALİÇESİNNN</div>
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        position: "relative",
+        display: "grid",
+        justifyContent: "center",
+        placeItems: "center",
+        alignItems: "center",
+        overflow: "hidden",
+      }}
+    >
+      <video
+        style={{ overflow: "hidden" }}
+        width="400%"
+        height="100%"
+        autoPlay
+        muted
+        loop
+        controls={false}
+      >
+        <source src={video} type="video/mp4" />
+      </video>
+    </div>
+  </div>
+  );
+}
+
+export default App;
